59f2efb21d5732284523278b5516fac51a4f69f7
sujoymodakgithub <modaksujoy74@gmail.com>
2023-04-25 14:32:49 +0000

    Traffic Light

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-let colorPickerContainer = document.getElementById("colorPickerContainer");
-let selectedColorHexCode = document.getElementById("selectedColorHexCode");
+let stopLightElement = document.getElementById("stopLight");
+let readyLightElement = document.getElementById("readyLight");
+let goLightElement = document.getElementById("goLight");
+let stopButtonElement = document.getElementById("stopButton");
+let readyButtonElement = document.getElementById("readyButton");
+let goButtonElement = document.getElementById("goButton");
 
-function colorOne() {
-  colorPickerContainer.style.backgroundColor = "#e0e0e0";
-  selectedColorHexCode.textContent = "#e0e0e0";
+function turnOnRed() {
+  stopLightElement.style.backgroundColor = "#cf1124";
+  readyLightElement.style.backgroundColor = "#4b5069";
+  goLightElement.style.backgroundColor = "#4b5069";
+  stopButtonElement.style.backgroundColor = "#cf1124";
+  readyButtonElement.style.backgroundColor = "#1f1d41";
+  goButtonElement.style.backgroundColor = "#1f1d41";
 }
 
-function colorTwo() {
-  colorPickerContainer.style.backgroundColor = "#6fcf97";
-  selectedColorHexCode.textContent = "#6fcf97";
+function turnOnYellow() {
+  stopLightElement.style.backgroundColor = "#4b5069";
+  readyLightElement.style.backgroundColor = "#f7c948";
+  goLightElement.style.backgroundColor = "#4b5069";
+  stopButtonElement.style.backgroundColor = "#1f1d41";
+  readyButtonElement.style.backgroundColor = "#f7c948";
+  goButtonElement.style.backgroundColor = "#1f1d41";
 }
 
-function colorThree() {
-  colorPickerContainer.style.backgroundColor = "#56ccf2";
-  selectedColorHexCode.textContent = "#56ccf2";
-}
-
-function colorFour() {
-  colorPickerContainer.style.backgroundColor = "#bb6bd9";
-  selectedColorHexCode.textContent = "#bb6bd9";
+function turnOnGreen() {
+  stopLightElement.style.backgroundColor = "#4b5069";
+  readyLightElement.style.backgroundColor = "#4b5069";
+  goLightElement.style.backgroundColor = "#199473";
+  stopButtonElement.style.backgroundColor = "#1f1d41";
+  readyButtonElement.style.backgroundColor = "#1f1d41";
+  goButtonElement.style.backgroundColor = "#199473";
 }
